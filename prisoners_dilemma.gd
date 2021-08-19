e9ae14093d2fa4bc35ebe5b0a3ba767f5f5567d8
Anjay Goel <anjaygoel@gmail.com>
2021-08-20 02:03:35 +0530

    Add Assets

--- prisoners_dilemma.gd
+++ prisoners_dilemma.gd
@@ -1,5 +1,9 @@
 extends Control
 
+
+const wait_time = 20;
+var curr_time = 0; 
+
 var curr_round = 1
 var p1_score = 0
 var p2_score = 0
@@ -17,9 +21,29 @@ onready var timer = $timer
 onready var you = $you
 onready var opp = $opp
 
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready():
-	pass # Replace with function body.
+   timer = Timer.new()
+   timer.connect("timeout",self,"_on_timer_timeout") 
+   timer.set_wait_time(1) #value is in seconds: 600 seconds = 10 minutes
+   timer.set_one_shot(false)
+   add_child(timer) 
+   timer.start() 
+
+
+func _on_timer_timeout():
+	curr_time += 1
+	$timer.set_text("%d"%(wait_time-curr_time))
+	if curr_time == wait_time:
+		curr_time = 0
+		if get_tree().is_network_server():
+			if (p1_move<0):
+				p1_move = 0
+			if (p2_move<0):
+				p2_move = 0
+			rpc("sync_state",p1_move,p2_move)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -39,7 +63,7 @@ func update_screen():
 func update_state():
 	print("Update state called")
 	print("p1: %s, p2: %s"%[p1_move,p2_move])
-	if (p1_move>=0 and p2_move>0):
+	if (p1_move>=0 and p2_move>=0):
 		#Confess,Confess
 		if(p1_move==0 and p2_move==0):
 			p1_score -=5
@@ -68,6 +92,7 @@ func update_state():
 		p1_move = -1
 		p2_move = -1
 		curr_round+=1
+		curr_time = 0
 		update_screen()
 
 
